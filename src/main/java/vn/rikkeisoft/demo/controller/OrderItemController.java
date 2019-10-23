ba0b723aa631a826532cbd425664a50a9ddb3223
QuangHa <quangha12thxd@gmail.com>
2019-10-23 17:02:10 +0700

    Update 1

--- src/main/java/vn/rikkeisoft/demo/controller/OrderItemController.java
+++ src/main/java/vn/rikkeisoft/demo/controller/OrderItemController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vn.rikkeisoft.demo.model.entity.Order;
 import vn.rikkeisoft.demo.repositories.OrderItemRepository;
+import vn.rikkeisoft.demo.repositories.OrderRepository;
 
 @Controller
 @RequestMapping("/order-items")
@@ -13,9 +16,14 @@ public class OrderItemController {
     @Autowired
     private OrderItemRepository orderItemRepository;
 
-    @GetMapping("/")
-    public String get(Model model) {
-        model.addAttribute("orderItemList", orderItemRepository.findAll());
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @GetMapping("/{id}")
+    public String get(Model model, @PathVariable("id") long id) {
+        Order order = orderRepository.findById(id).get();
+        model.addAttribute("orders", orderItemRepository.findAllByOrder(order));
+        model.addAttribute("idOther", id);
         return "orderItem";
     }
 }
--- src/main/java/vn/rikkeisoft/demo/repositories/OrderItemRepository.java
+++ src/main/java/vn/rikkeisoft/demo/repositories/OrderItemRepository.java
@@ -1,7 +1,11 @@
 package vn.rikkeisoft.demo.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import vn.rikkeisoft.demo.model.entity.Order;
 import vn.rikkeisoft.demo.model.entity.OrderItem;
 
+import java.util.List;
+
 public interface OrderItemRepository extends CrudRepository<OrderItem, Long> {
+    List<OrderItem> findAllByOrder(Order order);
 }
