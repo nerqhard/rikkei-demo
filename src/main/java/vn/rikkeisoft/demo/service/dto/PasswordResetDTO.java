4739d30ed12db80ca8780ecb8247a63c1c33dfbf
QuangHa <quangha12thxd@gmail.com>
2019-11-04 11:15:24 +0700

    view admin

--- src/main/java/vn/rikkeisoft/demo/service/dto/PasswordResetDTO.java
+++ src/main/java/vn/rikkeisoft/demo/service/dto/PasswordResetDTO.java
@@ -1,11 +1,15 @@
 package vn.rikkeisoft.demo.service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import vn.rikkeisoft.demo.common.PasswordMatches;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @PasswordMatches
 public class PasswordResetDTO {
     @NotEmpty
